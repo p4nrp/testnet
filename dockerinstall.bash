31851303f37badf4c49f5813485950b19e90fe1d
luthfan <55221206+p4nrp@users.noreply.github.com>
2023-10-26 17:22:57 +0700

    Update dockerinstall.bash

--- dockerinstall.bash
+++ dockerinstall.bash
@@ -17,23 +17,3 @@ apt-cache policy docker-ce
 
 # Install Docker
 sudo apt install -y docker-ce
-
-
-# Install necessary packages
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-
-# Add Docker GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-# Add Docker repository
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# Update package list
-sudo apt update
-
-# Check Docker version
-apt-cache policy docker-ce
-
-# Install Docker
-sudo apt install -y docker-ce
